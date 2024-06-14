31c619b34513df7e9ecb830a899d8b458dff60b6
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-06-15 00:25:54 +0500

    File of q136

---
+++ q136.js
@@ -0,0 +1,6 @@
+// Use console.log() to debug and track the value of a variable inside a loop.
+// Demonstrates using console.log() inside a loop to track variable values
+for (var i = 1; i <= 2; i++) {
+    console.log('Iteration ${i}, i value:,i'); // Logs the current iteration number and the value of i
+}
+// This loop iterates five times, logging the value of 'i' during each iteration to help with debugging.
